3b4585e01b0beb4f002d53a3ccca7b0cedc7705c
doughertybd <doughebd@mail.uc.edu>
2022-11-22 15:02:12 -0500

    final commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int numToExamine = 2;
+        if ((numToExamine % 2) == 0) {
+            System.out.println("the number " + numToExamine + " is even.");
+        }else{
+            System.out.println("the number " + numToExamine + " is odd.");
+        }
+    }
+}
